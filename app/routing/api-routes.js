d263978b57611dd0abaf6676d91cdb3de970588e
Kamy <kamy.leeret@gmail.com>
2020-04-27 16:19:47 -0600

    update api routes for friends (home page link working api/friends)

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,23 +1,28 @@
-// var friendsData = require('./data/friends.js');
+var friendsData = require('../data/friends.js');
 // var surveyData = require('./data/survey-data.js');
 
 var path = require('path');
 
 module.exports = function (app) {
  
+  //================== NAVIGATION =====================//
   app.get("/", function(request,result) {
     result.sendFile(path.join(__dirname, "../public/home.html"));
   })
-  
   app.get("/survey", function(request, result) {
     result.sendFile(path.join(__dirname, "../public/survey.html"));
   })
+  //================== END HTML NAVIGATION =============//
 
-  //display friends
+
+  //================== FRIENDS (DRAGONS) API DATA ================//
+  app.get("/api/friends", function (request, result) {
+    return result.json(friendsData);
+  });
   app.get("/api/:friends?", function (request, result) {
       var friendMatch = request.params.friends;
         if(friendMatch) {
-          console.log(matfriendMatchched);
+          console.log(friendMatch);
           for (var i = 0; i < friends.length; i++) {
             if (friendMatch === friends[i].routeName) {
               return result.json(friends[i]);
@@ -25,9 +30,23 @@ module.exports = function (app) {
                   }
                   result.send("No Match Found");
             } else {
-              result.json(friendMatch); 
+              result.json(friendsData); 
             };
     });
+    app.post("/api/friends", function (request, result) {
+      return result.json(friendsData)
+    });
+
+  //================== END FRIENDS (DRAGONS) DATA =============//
+
+
+  //================== SURVEY API DATA =====================//
+
+
+
+
+//================== END SURVEY DATA =====================//
+
 
 };
 
