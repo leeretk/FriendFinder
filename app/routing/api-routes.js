e27c73208cc321e6667015ab2b360184b5622e07
Kamy <kamy.leeret@gmail.com>
2020-04-27 16:03:27 -0600

    api routes are working for html buttons
    Survey will open Home, Home will open Survey.  Next Step.  Capture the data.

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,54 +1,33 @@
-var friendsData = require('./data/friends.js');
-var surveyData = require('./data/survey-data.js');
+// var friendsData = require('./data/friends.js');
+// var surveyData = require('./data/survey-data.js');
 
 var path = require('path');
 
 module.exports = function (app) {
  
-  //GET Survey 
-
-  app.get("/api/:survey?", function (request, result) {
-      var matched = request.params.survey;
-      if (matched) {
-        console.log(matched);
-        for (var i = 0; i < friends.length; i++) {
-          if (matched === survey[i].name) {
-            return result.json(survey[i]);
-          }
-        }
-        result.send("Dragon Not Found");
-      } else {
-        result.json(surveyData);
-      };
-  });
-  //POST Survey Data
-  app.post("/api/survey", function (request, result) {
-     return result.json(surveyData)
+  app.get("/", function(request,result) {
+    result.sendFile(path.join(__dirname, "../public/home.html"));
+  })
+  
+  app.get("/survey", function(request, result) {
+    result.sendFile(path.join(__dirname, "../public/survey.html"));
   })
 
-  //==========================================================//
-
-  //GET Friends (get dragon)
-  app.get("/api/friends", function (request, result) {
-    return result.json(friendsData);
-  });
+  //display friends
   app.get("/api/:friends?", function (request, result) {
-    var matched = request.params.friends;
-    if (matched) {
-      console.log(matched);
-      for (var i = 0; i < friends.length; i++) {
-        if (matched === friends[i].name) {
-          return result.json(friends[i]);
-        }
-      }
-      result.send("Dragon Not Found");
-    } else {
-      result.json(friendsData);
-    };
-  });
-  // POST Friends Data (dragon data)
-  app.post("/api/friends", function (request, result) {
-    return result.json(friendsData)
-  })
+      var friendMatch = request.params.friends;
+        if(friendMatch) {
+          console.log(matfriendMatchched);
+          for (var i = 0; i < friends.length; i++) {
+            if (friendMatch === friends[i].routeName) {
+              return result.json(friends[i]);
+            }
+                  }
+                  result.send("No Match Found");
+            } else {
+              result.json(friendMatch); 
+            };
+    });
+
 };
 
