3d0dde5d83c03184f4a81467cf9645be3bfc4a33
Kamy <kamy.leeret@gmail.com>
2020-04-27 18:59:29 -0600

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,61 +4,71 @@ var friendsArray = [
     {
       routeName: "norwegianridgeback",
       name: "norwegian-ridgeback",
-      photo: "href=/1_norwegian-ridgeback.jpg",
-      scores: [3,5,2,3,1,3,1,1,5,2]
+      photo: "href= ../assets/images/1_norwegian-ridgeback.jpg",
+      scores: [4,5,2,3,1,3,1,1,5,2],
+      totalScore: 27,
     }, {
       routeName: "chinesefireball",
       name: "chinese-fireball",
-      photo: "href=/2_chinese-fireball.jpg",
-      scores: [5,3,4,1,5,2,5,1,2,2]
+      photo: "href= ../assets/images/2_chinese-fireball.jpg",
+      scores: [5,3,4,1,5,2,5,1,2,2],
+      totalScore: 30,
     },
     {
       routeName: "ukranianironbelly",
       name: "ukranian-ironbelly",
-      photo: "href=/3_ukranian-ironbelly.jpg",
-      scores: [3,2,2,2,2,2,3,1,5,4]
+      photo: "href= ../assets/images/3_ukranian-ironbelly.jpg",
+      scores: [3,2,2,2,2,2,3,1,5,4],
+      totalScore: 26,
     },
     {
       routeName: "hungarianhorntail",
       name: "hungarian-horntail",
-      photo: "href=/4_hungarian-horntail.jpg",
-      scores: [3,2,2,2,2,2,3,1,5,4] 
+      photo: "href= ../assets/images/4_hungarian-horntail.jpg",
+      scores: [4,3,3,3,2,4,4,1,5,4],
+      totalScore: 33,
     },
     {
       routeName: "smaug",
       name: "smaug",
-      photo: "href=/5_smaug.jpg",
-      scores:  [5,5,5,5,5,2,4,2,2,3]
+      photo: "href= ../assets/images/5_smaug.jpg",
+      scores:[5,5,5,5,5,2,4,2,2,3],
+      totalScore: 38,
     },
     {
       routeName: "nightfury",
       name: "night-fury",
-      photo: "href=/6_night-fury.jpg",
-      scores: [2,2,1,2,3,5,4,5,1,5]
+      photo: "href= ../assets/images/6_night-fury.jpg",
+      scores:[3,3,1,2,3,5,4,5,1,5],
+      totalScore: 32,
     },
     {
       routeName: "puffmagicdragon",
       name: "puff-magic-dragon",
-      photo: "href=/7_puff-magic.jpg",
-      scores: [1,1,2,3,3,4,2,4,5,3]
+      photo: "href= ../assets/images/7_puff-magic.jpg",
+      scores:[2,1,2,3,3,4,2,4,5,3],
+      totalScore: 29,
     },
     {
       routeName: "petesdragon",
       name: "petes-dragon",
-      photo: "href=/8_petes-dragon.jpg",
-      scores:  [2,1,1,4,4,1,4,4,1,3]
+      photo: "href= ../assets/images/8_petes-dragon.jpg",
+      scores:  [2,1,1,4,4,1,4,4,1,3],
+      totalScore: 25,
     },
     {
       routeName: "drogon",
       name: "drogon",
-      photo: "href=/9_drogon.jpg",
-      scores:  [4,5,4,1,1,1,2,4,3,4]
+      photo: "href= ../assets/images/9_drogon.jpg",
+      scores:[5,5,5,1,1,1,2,4,3,4],
+      totalScore: 31,
     },
     {
       routeName: "mushu",
       name: "mushu",
-      photo: "href=/10_mushu.jpg",
-      scores:  [1,1,3,5,4,5,2,5,1,2]
+      photo: "href= ../assets/images/10_mushu.jpg",
+      scores:[1,1,3,5,4,5,2,4,1,2],
+      totalScore: 28,
     }];
 
     module.exports = friendsArray;
