1983ca8f24ede7301b6a841d15dcbd579c96e92e
Kamy <kamy.leeret@gmail.com>
2020-04-29 17:59:41 -0600

    updates but not working.

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,58 +1,58 @@
 var friendsData = require('../data/friends.js');
-
 var path = require('path');
 
 module.exports = function (app) {
  
-  //================== NAVIGATION =====================//
-  app.get("/", function(request,result) {
-    result.sendFile(path.join(__dirname, "../public/home.html"));
+  app.get("/", function(request,response) {
+    response.sendFile(path.join(__dirname, "../public/home.html"));
   })
-  app.get("/survey", function(request, result) {
-    result.sendFile(path.join(__dirname, "../public/survey.html"));
+  app.get("/survey", function(request, response) {
+    response.sendFile(path.join(__dirname, "../public/survey.html"));
   })
-  //================== END HTML NAVIGATION ===============//
 
   //================== FRIENDS (DRAGONS) API DATA ========//
-    app.get("/api/friends", function (request, result) {
-      return result.json(friendsData);
-    });
+    app.get("/api/friends", function (request, response) {
+        response.json(friendsData);
+        } 
+    );
     
-    app.post("/api/friends", function ({body}, res) {
-    
-        console.log(body.scores);
-        
-        var surveyInfo = body;
-        
-        console.log(body);
-        console.log(surveyInfo.scores);
+    app.post("/api/friends", function (request, response) {
+                
+      var surveyInfo = request.body;
 
-    // parsInt for scores
-    for (var i=0; i < surveyInfo.scores.length; i++) {
-        surveyInfo.scores[i] = parseInt(surveyInfo.scores[i]);
-    }
+      for (var i=0; i < surveyInfo.scores.length; i++) {
+                surveyInfo.scores[i] = parseInt(surveyInfo.scores[i]);
+            };
+              var matchIndex = 0;
+              var minDifference = 20; 
 
-    var matchIndex = 0;
-    var minDifference = 20; 
+              for(var i = 0; i < friendsData.length; i++) {
 
-    for(var i = 0; i < friends.length; i++) {
+              var totalDifference = 0;
 
-      var totalDifference = 0;
+              for(var b = 0; b < friendsData[i].scores.length; b++) {
 
-      for(var b = 0; b < friends[i].scores.length; b++) {
-        var difference = Math.abs(surveyInfo.scores[b] - friends[i].scores[b])
-        totalDifference += difference;
-      }
-      if(totalDifference < minDifference) {
-        matchIndex = i;  //keeping track of the friend winning so far.
-        minDifference = totalDifference;  //keep track of the smallest diff between user and friend list.
-      }
-    }
-    friends.push(surveyInfo);
-    console.log(friends[matchIndex]);
-    response.json(friends[matchIndex]);
+                var difference = Math.abs(surveyInfo.scores[b] - friendsData[i].scores[b])
+                totalDifference += difference;
+              }
+              console.log(surveyInfo.body);
+              console.log(surveyInfo.scores);
+
+              if(totalDifference < minDifference) {
+                matchIndex = i;  //keeping track of the friend winning so far.
+                minDifference = totalDifference;  //keep track of the smallest diff between user and friend list.
+              }
+              }
+    friendsData.push(surveyInfo);
+    console.log(friendsData[matchIndex]);
+    response.json(friendsData[matchIndex]);
   });
   //================== END SURVEY DATA =====================//
+
+  app.post("/api/clear", function(request, response) {
+    surveyInfo.length = 0;
+    response.json({ ok: true });
+  });
 
 };
 
