8b379b699527578507c0901b7cb2cfc0441b538f
Kamy <kamy.leeret@gmail.com>
2020-05-06 21:06:54 -0600

    this better work for the images in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,65 +2,65 @@
 //DRAGON DATA
 // *********************************************************************************
 var friendsData = [
-  {
-      name: 'norwegian-ridgeback',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/1norwegian-ridgeback.jpg',
-      scores: [4, 5, 2, 3, 1, 3, 1, 1, 5, 2],
-  },
-  {
-      name: 'chinese-fireball',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/2_chinese-fireball.jpg',
-      scores: [5, 3, 4, 1, 5, 2, 5, 1, 2, 2],
-  },
-  {
-      name: 'ukranian-ironbelly',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/3_ukranian-ironbelly.jpg',
-      scores: [3, 2, 2, 2, 2, 2, 3, 1, 5, 4],
-  },
-  {
-      name: 'hungarian-horntail',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/4_hungarian-horntail.jpg',
-      scores: [4, 3, 3, 3, 2, 4, 4, 1, 5, 4],
-  },
-  {
-      name: 'smaug',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/5_smaug.jpg',
-      scores: [5, 5, 5, 5, 5, 2, 4, 2, 2, 3],
-  },
-  {
-      name: 'night-fury',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/6_night-fury.jpg',
-      scores: [3, 3, 1, 2, 3, 5, 4, 5, 1, 5],
-  },
-  {
-      name: 'puff-the-magic-dragon',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/7_puff-magic.jpg',
-      scores: [2, 1, 2, 3, 3, 4, 2, 4, 5, 3],
-  },
-  {
-      name: 'petes-dragon',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/8_petes-dragon.jpg',
-      scores: [2, 1, 1, 4, 4, 1, 4, 4, 1, 3],
-  },
-  {
-      name: 'drogon',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/9_drogon.jpg',
-      scores: [5, 5, 5, 1, 1, 1, 2, 4, 3, 4],
-  },
-  {
-      name: 'mushu',
-      email: 'kamy@leeret.com',
-      photo: '../public/images/10_mushu.jpg',
-      scores: [1, 1, 3, 5, 4, 5, 2, 4, 1, 2],
-  },
+    {
+        name: 'norwegian-ridgeback',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/1_norwegian-ridgeback.jpg',
+        scores: [4, 5, 2, 3, 1, 3, 1, 1, 5, 2],
+    },
+    {
+        name: 'chinese-fireball',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/2_chinese-fireball.jpg',
+        scores: [5, 3, 4, 1, 5, 2, 5, 1, 2, 2],
+    },
+    {
+        name: 'ukranian-ironbelly',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/3_ukranian-ironbelly.jpg',
+        scores: [3, 2, 2, 2, 2, 2, 3, 1, 5, 4],
+    },
+    {
+        name: 'hungarian-horntail',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/4_hungarian-horntail.jpg',
+        scores: [4, 3, 3, 3, 2, 4, 4, 1, 5, 4],
+    },
+    {
+        name: 'smaug',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/5_smaug.jpg',
+        scores: [5, 5, 5, 5, 5, 2, 4, 2, 2, 3],
+    },
+    {
+        name: 'night-fury',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/6_night-fury.jpg',
+        scores: [3, 3, 1, 2, 3, 5, 4, 5, 1, 5],
+    },
+    {
+        name: 'puff-the-magic-dragon',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/7_puff-magic.jpg',
+        scores: [2, 1, 2, 3, 3, 4, 2, 4, 5, 3],
+    },
+    {
+        name: 'petes-dragon',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/8_petes-dragon.jpg',
+        scores: [2, 1, 1, 4, 4, 1, 4, 4, 1, 3],
+    },
+    {
+        name: 'drogon',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/9_drogon.jpg',
+        scores: [5, 5, 5, 1, 1, 1, 2, 4, 3, 4],
+    },
+    {
+        name: 'mushu',
+        email: 'kamy@leeret.com',
+        photo: 'https://raw.githubusercontent.com/leeretk/FriendFinder/master/assets/images/10_mushu.jpg',
+        scores: [1, 1, 3, 5, 4, 5, 2, 4, 1, 2],
+    },
 ];
 module.exports = friendsData;
